428549338be26831b9c1fb77c6750c66fa648d88
jy <243950661@qq.com>
2020-09-14 17:22:11 +0800

    2

--- src/main/java/com/bookee/guidetest/CollectionsTest1.java
+++ src/main/java/com/bookee/guidetest/CollectionsTest1.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class CollectionTest {
+public class CollectionsTest1 {
     public static void main(String[] args) {
         List<Integer> arrayList = new ArrayList<>();
         arrayList.add(-1);
---
+++ src/main/java/com/bookee/guidetest/CollectionsTest2.java
@@ -0,0 +1,37 @@
+package com.bookee.guidetest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionsTest2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(-1);
+        arrayList.add(3);
+        arrayList.add(3);
+        arrayList.add(-5);
+        arrayList.add(7);
+        arrayList.add(4);
+        arrayList.add(-9);
+        arrayList.add(-7);
+        ArrayList<Integer> arrayList2 = new ArrayList<Integer>();
+        arrayList2.add(-3);
+        arrayList2.add(-5);
+        arrayList2.add(7);
+
+        System.out.println(arrayList);
+
+        //找出最大的元素值
+        System.out.println(Collections.max(arrayList));
+
+        //用新元素替换指定元素
+        Collections.replaceAll(arrayList,3,-3);
+        System.out.println(arrayList);
+
+        //统计元素出现的次数
+        System.out.println(Collections.frequency(arrayList,-3));
+
+        Collections.fill(arrayList,5);
+        System.out.println(arrayList);
+    }
+}
